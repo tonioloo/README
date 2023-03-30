fab01c81dcb6b2e1a71f7c9709656606b518a009
tonioloo <125101739+tonioloo@users.noreply.github.com>
2023-03-30 20:16:48 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+//Exemplo de código para manipular elementos HTML em JavaScript
+
+//selecionando o elemento h1
+let h1 = document.querySelector('h1');
+
+//adicionando um evento de clique ao elemento h1
+h1.addEventListener('click', function() {
+  alert('Bem-vindo ao repositório da nossa agência de marketing e gestão de mídias sociais!');
+});
